c06face1982914963306b8a4a72679bcaf3e7c1e
Yasul, Berkan <s65891@beuth-hochschule.de>
2016-06-23 18:01:51 +0200

    class diagram almost finished?

--- src/Field.java
+++ src/Field.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Thundersama on 23.06.2016.
  */
 public class Field {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        JPanel field = new JPanel(new GridLayout());
+        frame.add(field);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 480));
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
