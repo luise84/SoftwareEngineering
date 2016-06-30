b3dcc1bacec81a2ccbebc952cd8ed27e2b04a380
Luise <luise.lesui@gmail.com>
2016-06-30 17:12:24 +0200

    Stone and Field updated

--- src/game/Field.java
+++ src/game/Field.java
@@ -12,8 +12,47 @@ public class Field{
     }
 
     public boolean setPosition(Stone stone, int x, int y){
-        return true;
+        if(field[x][y] == null){
+            field[x][y]= stone;
+            return true;
+        }
+
+        else{
+            return false;
+        }
+
+
     }
+    /*
+    stone has to have the right combination of right movementtype and movedirection
+     */
+    private boolean checkMovement(Stone stone, int x, int y){
+        //stone can jump
+        if(stone.movementType){
+           //@ToDO check if new field in (after next row or after next column) or (in after next row and after next column)
+    // condition: leveltype!!!
+                // field is free
+                if(field[x][y] == null) return true;
+
+                else return false;
+
+        }
+
+        //stone can only move
+        else{
+            //@ToDO check if new field in next row or in next column
+            //condition: leveltype!!!
+            if(field[x][y] == null){
+                //set MoveNumber
+                stone.setMovementType();
+                return true;
+            }
+            else return false;
+
+        }
+    }
+
+
 
 
     private void notifyView(){
@@ -21,7 +60,8 @@ public class Field{
     }
 
     private boolean endGame(){
-        return false;
+
+        return true;
     }
 
 
--- src/game/Stone.java
+++ src/game/Stone.java
@@ -7,8 +7,31 @@ public class Stone {
 
 	private boolean affiliation;
 	private String color;
+	//movementType: true = jump, false = move
+	public boolean movementType;
+	//moveNumber = 0 -> jump, 1 = first move done, 2 = second move done
+	public int moveNumber = 0;
+	public void setMovementType() {
+		if(movementType) movementType = false;
+		else movementType = true;
+	}
+
+	public void setMoveNumber(){
+		if(moveNumber == 0 || moveNumber == 1){
+			moveNumber++;
+			return;
+		}
+		else if(moveNumber == 2){
+			moveNumber = 0;
+			return;
+		}
+	}
+
+
+
 
 	public Stone(boolean affiliation) {
+		this.movementType = true;
 		this.affiliation = affiliation;
 	}
 
