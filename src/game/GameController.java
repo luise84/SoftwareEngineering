60781acb8ead739877e1c2214c1c7d81830e1a38
Luise <luise.lesui@gmail.com>
2016-07-01 10:33:29 +0200

    field, stone and gamecontroller updated

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -9,37 +9,37 @@ import playfield.GameSetup;
  * Created by Luise on 23.06.2016.
  */
 public class GameController {
-	private Field field;
-	private String level;
+	private static Field field;
+	private static String level;
 	private AIPlayer aiplayer;
-	private GameSetup gamesetup;
+	private static GameSetup gamesetup;
 
 	public GameController() {
 		this.gamesetup = new GameSetup();
 		aiplayer = new AIPlayer();
 	}
 
-	/*private boolean setMove(Stone stone, int x, int y){
+	public boolean setMove(Stone stone, int x, int y){
 		if(field.setPosition(stone,x,x)) return true;
 		else return false;
-	}*/
+	}
 	private void waitForPlayerInput(){
 
+		computerMove();
 	}
 
-	private void userMove(){
 
-	}
 
 	private void computerMove(){
 		aiplayer.calculateMove();
+		waitForPlayerInput();
 	}
 
 
 
 
 
-	private void startGame(){
+	private static void startGame(){
 		field = gamesetup.chooseField();
 		level = gamesetup.chooseLevel();
 
@@ -47,9 +47,9 @@ public class GameController {
 	}
 	private void setup(){}
 
-	public void main(String args[]){
+	public static void main(String args[]){
 		startGame();
-		userMove();
+		//userMove();
 
 	}
 }
