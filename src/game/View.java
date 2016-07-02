d59fb5aea5ac5952050bbffbd88be2f6fe3da31d
Yasul, Berkan <s65891@beuth-hochschule.de>
2016-07-02 03:07:22 +0200

    Movement restriction for straight levels now working, allowed fields are highlighted, skipping over stones as next goal

--- src/game/View.java
+++ src/game/View.java
@@ -4,6 +4,7 @@ import gameConfigurations.Attribute;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -11,6 +12,7 @@ import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Luise on 23.06.2016.
@@ -22,9 +24,13 @@ public class View extends JFrame implements MouseListener,MouseMotionListener {
 	JLayeredPane layeredPane;
 	JPanel chessBoard;
 	JLabel chessPiece;
-	int field_count = 64;
+	int field_count = 81;
+	String movement = "straight";
 	int xAdjustment;
 	int yAdjustment;
+	Point old_loc;
+	Color fieldColor = Color.ORANGE;
+	ArrayList<JPanel> allowed_fields = new ArrayList<>();
 
 	public View(){
 		Dimension boardSize = new Dimension(600, 600);
@@ -39,22 +45,23 @@ public class View extends JFrame implements MouseListener,MouseMotionListener {
 		//Add a chess board to the Layered Pane
 		chessBoard = new JPanel();
 		layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
-		chessBoard.setLayout( new GridLayout(8, 8) );
+		chessBoard.setLayout( new GridLayout(9, 9) );
 		chessBoard.setPreferredSize( boardSize );
 		chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
 
 		for (int i = 0; i < field_count; i++) {
 			JPanel square = new JPanel( new BorderLayout() );
+			square.setBorder(BorderFactory.createLineBorder(Color.black));
 			chessBoard.add( square );
 
 			int row = (i / 8) % 2;
-			if (row == 0)
+			square.setBackground(fieldColor);
+			/*if (row == 0)
 				square.setBackground( i % 2 == 0 ? Color.YELLOW : Color.ORANGE);
 			else
 				square.setBackground( i % 2 == 0 ? Color.ORANGE: Color.YELLOW);
+				*/
 		}
-
-		//Add a few pieces to the board
 
 		//@todo at this point the own stone class might be needed
 
@@ -75,8 +82,8 @@ public class View extends JFrame implements MouseListener,MouseMotionListener {
 		ImageIcon icon_w = new ImageIcon(dimg_w);
 
 
-
-		for(int i = 0; i<16; i++){
+		//number of stones must be dependend on board type
+		for(int i = 0; i<18; i++){
 			JLabel piece_b = new JLabel(icon_b);
 			JPanel panel_b = (JPanel)chessBoard.getComponent(i);
 			panel_b.add(piece_b);
@@ -127,8 +134,9 @@ public class View extends JFrame implements MouseListener,MouseMotionListener {
         frame.setPreferredSize(new Dimension(800, 800));
         //frame.pack();
         //frame.setVisible(true);
-        */
-		JFrame frame = new JFrame();
+*/
+
+		View frame = new View();
 		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE );
 		frame.pack();
 		frame.setResizable(true);
@@ -142,16 +150,103 @@ public class View extends JFrame implements MouseListener,MouseMotionListener {
 
 	}
 
+	public ArrayList<JPanel> getAllowedMoves(JPanel currentField, Point location){
+		Dimension dim = currentField.getSize();
+		System.out.println(location);
+
+
+		//hardcoded allowed moves for gamemode straight
+		ArrayList<Component> possible_fields = new ArrayList<>();
+		possible_fields.add(chessBoard.findComponentAt(location.x , location.y - dim.height));
+		possible_fields.add(chessBoard.findComponentAt(location.x , location.y + dim.height));
+		possible_fields.add(chessBoard.findComponentAt(location.x - dim.width, location.y ));
+		possible_fields.add(chessBoard.findComponentAt(location.x + dim.height, location.y ));
+
+		//Component c_=  chessBoard.findComponentAt(location.x , location.y - dim.height);
+		ArrayList<JPanel> ret_fields = new ArrayList<>();
+		for(Component field : possible_fields){
+			if(field instanceof JPanel){
+				field.setBackground(Color.GREEN);
+				ret_fields.add((JPanel)field);
+			}else if (field instanceof JLabel){
+				//if there already is another piece you can skip
+			}
+		}
+		return ret_fields;
+	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {
+		chessPiece = null;
+		Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+		if (c instanceof JPanel){
+			return;
+		}
+		Point parentLocation = c.getParent().getLocation();
+
+		//calculate the allowed moves based on the selected Figure
+		JPanel field = (JPanel)c.getParent();
+
+		allowed_fields = getAllowedMoves(field,new Point(e.getX(),e.getY()));
+
+
+		// save the initial position in order to reset if necessary
+		old_loc = parentLocation;
+		xAdjustment = parentLocation.x - e.getX();
+		yAdjustment = parentLocation.y - e.getY();
+		chessPiece = (JLabel)c;
+
+		chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
+		chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+		layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
 
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
+		for(JPanel field : allowed_fields){
+			field.setBackground(fieldColor);
+		}
+
+		if(chessPiece == null){
+			return;
+		}
+
+		chessPiece.setVisible(false);
+		Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+
+		//if there is another stone return the dragged stone to its initial place
+		if (c instanceof JLabel){
+			JPanel old_panel = (JPanel)chessBoard.findComponentAt(old_loc);
+			old_panel.add(chessPiece);
+		}
+		else {
+			boolean move_check = false;
+			for(JPanel field : allowed_fields){
+				if(((Component)field).equals(c)){
+					move_check = true;
+				}
+			}
+			if(move_check){
+				Container parent = (Container)c;
+				parent.add( chessPiece );
+			}else{
+				JPanel old_panel = (JPanel)chessBoard.findComponentAt(old_loc);
+				old_panel.add(chessPiece);
+			}
+
+		}
+
+		chessPiece.setVisible(true);
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if (chessPiece == null){
+			return;
+		}
+		chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
 
 	}
-
 	@Override
 	public void mouseEntered(MouseEvent e) {
 
@@ -162,10 +257,7 @@ public class View extends JFrame implements MouseListener,MouseMotionListener {
 
 	}
 
-	@Override
-	public void mouseDragged(MouseEvent e) {
 
-	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
